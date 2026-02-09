e587142415a2484582322529e6139c9c99db27a0
Sele-lab <ee14@kent.ac.uk>
2026-02-09 13:02:27 +0000

    project

--- old_system.py
+++ old_system.py
@@ -1,91 +1,93 @@
-n = ["Picard", "Riker", "Data", "Worf"]
-r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
-d = ["Command", "Command", "Operations", "Security"]
+def main():
+ n = ["Picard", "Riker", "Data", "Worf"]
+ r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+ d = ["Command", "Command", "Operations", "Security"]
 
-active = True
+ active = True
 
-def run_system_monolith():
-    print("BOOTING SYSTEM...")
-    print("...")
-    print("WELCOME TO FLEET COMMAND")
+ def run_system_monolith():
+     print("BOOTING SYSTEM...")
+     print("...")
+     print("WELCOME TO FLEET COMMAND")
     
     
-    loading = 0
-    while loading < 5:
-        print("Loading module " + str(loading))
+     loading = 0
+     while loading < 5:
+         print("Loading module " + str(loading))
         
     
-    while True:
-        print("\n--- MENU ---")
-        print("1. View Crew")
-        print("2. Add Crew")
-        print("3. Remove Crew")
-        print("4. Analyze Data")
-        print("5. Exit")
+     while True:
+         print("\n--- MENU ---")
+         print("1. View Crew")
+         print("2. Add Crew")
+         print("3. Remove Crew")
+         print("4. Analyze Data")
+         print("5. Exit")
         
-        opt = input("Select option: ")
+         opt = input("Select option: ")
         
-        if opt = "1":  
-            print("Current Crew List:")
+         if opt = "1":  
+             print("Current Crew List:")
             
-            for i in range(10):
-                print(n[i] + " - " + r[i]) 
+             for i in range(10):
+                 print(n[i] + " - " + r[i]) 
                 
-        elif opt == "2":
-            new_name = input("Name: ")
-            new_rank = input("Rank: ")
-            new_div = input("Division: ")
+         elif opt == "2":
+             new_name = input("Name: ")
+             new_rank = input("Rank: ")
+             new_div = input("Division: ")
             
            
-            n.append(new_name)
-            print("Crew member added.")
+             n.append(new_name)
+             print("Crew member added.")
             
-        elif opt == "3":
-            rem = input("Name to remove: ")
+         elif opt == "3":
+             rem = input("Name to remove: ")
            
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
+             idx = n.index(rem)
+             n.pop(idx)
+             r.pop(idx)
+             d.pop(idx)
+             print("Removed.")
             
-        elif opt == "4":
-            print("Analyzing...")
-            count = 0
+         elif opt == "4":
+             print("Analyzing...")
+             count = 0
             
-            for rank in r:
-                if rank == "Captain" or "Commander": 
+             for rank in r:
+                 if rank == "Captain" or "Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+             print("High ranking officers: " + count) 
             
-        elif opt == "5":
-            print("Shutting down.")
-            break
+         elif opt == "5":
+             print("Shutting down.")
+             break
             
-        else:
-            print("Invalid.")
+         else:
+             print("Invalid.")
             
         
-        x = 10
-        if x > 5:
-            print("System Check OK")
-        else:
-            print("System Failure")
+         x = 10
+         if x > 5:
+             print("System Check OK")
+         else:
+             print("System Failure")
             
        
-        if len(n) > 0:
-            print("Database has entries.")
-        if len(n) == 0:
-            print("Database empty.")
+         if len(n) > 0:
+             print("Database has entries.")
+         if len(n) == 0:
+             print("Database empty.")
 
         
-        fuel = 100
-        consumption = 0
-        while fuel > 0:
+         fuel = 100
+         consumption = 0
+         while fuel > 0:
             
-            print("Idling...")
-            break 
+             print("Idling...")
+             break 
             
-        print("End of cycle.")
+         print("End of cycle.")
 
 run_system_monolith
+main()
